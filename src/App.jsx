b1356882f07f8e863831f6ee578721b0fba0148e
MonaMohammed <mona00mona1997@gmail.com>
2025-05-15 14:14:37 +0300

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useRef, useState } from "react";
-import * as XLSX from "xlsx";
-import "./App.css";
-import Table from "./Table";
+import React, { useRef, useState, useEffect } from "react";
+
 function App() {
   const [currentTable, setCurrentTable] = useState([]);
   const [titleBtn, setTitleBtn] = useState("Import Excel File");
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    const dataTableFromLocalStorage = localStorage.getItem("dataTable");
+    if (dataTableFromLocalStorage) {
+      setCurrentTable(JSON.parse(dataTableFromLocalStorage));
+      setTitleBtn("Import and Analyze");
+    }
+  }, []);
+
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
     if (!file) return;
@@ -17,15 +24,18 @@ function App() {
       const worksheet = workbook.Sheets[workbook.SheetNames[0]];
       const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
       setCurrentTable(jsonData);
+      localStorage.setItem("dataTable", JSON.stringify(jsonData));
     };
     e.target.value = null;
 
     reader.readAsArrayBuffer(file);
     setTitleBtn("Import and Analyze");
   };
+
   const handleClick = () => {
     inputRef.current.click();
   };
+
   return (
     <div className="container" style={{ padding: "20px" }}>
       <h3 className="cursor-pointer" onClick={handleClick}>
