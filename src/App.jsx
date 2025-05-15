6618b81acaf1c7d0688970e4061f010aa3cca1fe
MonaMohammed <mona00mona1997@gmail.com>
2025-05-15 13:51:17 +0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from "react";
+import * as XLSX from "xlsx";
+import "./App.css";
+import Table from "./Table";
+function App() {
+  const [currentTable, setCurrentTable] = useState([]);
+  const [titleBtn, setTitleBtn] = useState("Import Excel File");
+  const inputRef = useRef(null);
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const data = evt.target.result;
+      const workbook = XLSX.read(data, { type: "array" });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
+      setCurrentTable(jsonData);
+    };
+    e.target.value = null;
+
+    reader.readAsArrayBuffer(file);
+    setTitleBtn("Import and Analyze");
+  };
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+  return (
+    <div className="container" style={{ padding: "20px" }}>
+      <h3 className="cursor-pointer" onClick={handleClick}>
+        {titleBtn}
+      </h3>
+      <input
+        ref={inputRef}
+        type="file"
+        accept=".xlsx, .xls"
+        style={{ display: "none" }}
+        onChange={handleFileUpload}
+      />
+      <Table data={currentTable} />
+    </div>
+  );
+}
+
+export default App;
